e4821459d0e5458a8c45ba25812dfc1dbfeee290
Mathboss42 <115639944+Mathboss42@users.noreply.github.com>
2023-03-20 18:04:19 +0100

    Set up Firebase and add image fetching

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import './App.css';
+import { initializeApp } from 'firebase/app';
+import { getFirebaseConfig } from './firebase-config.js';
+import { getFireStore, collection, getDocs } from 'firebase/firestore/lite'
+import { getDownloadURL, getStorage, ref } from "firebase/storage";
+import { useEffect, useState } from 'react';
+
+const app = initializeApp(getFirebaseConfig());
+// const db = getFireStore(app);
+const storage = getStorage(app);
+const pathReference = ref(storage, 'bg01.png');
 
 function App() {
-  return (
-    <div className="App">
+    const [bgImg, setBgImg] = useState('');
 
-    </div>
-  );
+    useEffect(() => {
+        getDownloadURL(pathReference).then((url) => {
+            setBgImg(url);
+        });
+    }, [])
+
+    return (
+        <div className="App">
+            {}
+            <img src={bgImg} alt="background" />
+        </div>
+    );
 }
 
 export default App;
