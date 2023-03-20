4b2e5330dce5e28dead2a191db505625f39cabaf
Mathboss42 <115639944+Mathboss42@users.noreply.github.com>
2023-03-20 18:49:13 +0100

    backup

--- src/App.js
+++ src/App.js
@@ -5,10 +5,27 @@ import { getFireStore, collection, getDocs } from 'firebase/firestore/lite'
 import { getDownloadURL, getStorage, ref } from "firebase/storage";
 import { useEffect, useState } from 'react';
 
+const GRIDX = 107;
+const GRIDY = 200;
+
 const app = initializeApp(getFirebaseConfig());
 // const db = getFireStore(app);
 const storage = getStorage(app);
 const pathReference = ref(storage, 'bg01.png');
+
+function generateGrid(length, height) {
+    const arr = [];
+
+    for (let i = 0; i < height; i++) {
+        for (let j = 0; j < length; j++) {
+            arr.push([j+1, i+1]);
+        }
+    }
+
+    return arr.map((el, index) => {
+        return <div className='grid-cell' data-x={el[0]} data-y={el[1]} key={index}></div>
+    })
+}
 
 function App() {
     const [bgImg, setBgImg] = useState('');
@@ -21,8 +38,10 @@ function App() {
 
     return (
         <div className="App">
-            {}
-            <img src={bgImg} alt="background" />
+            <div className="grid-container">
+                {generateGrid(GRIDX, GRIDY)}
+                <img src={bgImg} alt="background" />
+            </div>
         </div>
     );
 }
